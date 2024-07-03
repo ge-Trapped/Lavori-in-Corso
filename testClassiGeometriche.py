e49d4fb4f203cee2224f7b872c3d00dcc8ecccc3
ge-Trapped <santabarbara.sergio@gmail.com>
2024-07-03 09:55:45 +0200

    Create testClassiGeometriche.py

---
+++ testClassiGeometriche.py
@@ -0,0 +1,8 @@
+import pytest
+import classiGeometriche
+
+def testPoligono():
+    # 1. Creo un oggetto di tipo Poligono
+    # 2. Testo le sue funzonalità (i suoi metodi, i suoi attributi, i suoi metodi dunder ed il suo tipo)
+    # NB: ricorda che puoi verificare con un assert il tipo di un oggetto con la funzione
+    ...
