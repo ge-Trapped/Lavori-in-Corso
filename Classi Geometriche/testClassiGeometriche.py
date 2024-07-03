f1a3585f4023b4d416c1cc6c8e83d8329b93e889
ge-Trapped <santabarbara.sergio@gmail.com>
2024-07-03 10:01:14 +0200

    Update testClassiGeometriche.py

--- Classi Geometriche/testClassiGeometriche.py
+++ Classi Geometriche/testClassiGeometriche.py
@@ -4,5 +4,5 @@ import classiGeometriche
 def testPoligono():
     # 1. Creo un oggetto di tipo Poligono
     # 2. Testo le sue funzonalità (i suoi metodi, i suoi attributi, i suoi metodi dunder ed il suo tipo)
-    # NB: ricorda che puoi verificare con un assert il tipo di un oggetto con la funzione instance
+    # NB: ricorda che puoi verificare con un assert il tipo di un oggetto con la funzione isinstance
     ...
